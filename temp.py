ecf4741b4593b059fea1fab38825cbdcdc746fb6
kapoorarpit <kapoorarpit2000@gmail.com>
2021-07-27 22:15:14 +0530

    _

--- temp.py
+++ temp.py
@@ -17,47 +17,6 @@ driver =webdriver.Chrome(path)
 url = "https://www.google.com/search?q=restaurants+in+bareilly&oq=restaurants+in+bareilly&aqs=chrome..69i57.22434j0j9&sourceid=chrome&ie=UTF-8"
 driver.get(url)
  
-'''try:
-    link = WebDriverWait(driver, 40).until(
-        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "web results for "))
-    )
-    link.click()
-    
-    view_all = WebDriverWait(driver, 40).until(
-        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "View all"))
-    )
-    view_all.click()
-    main = WebDriverWait(driver, 90).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "rl_tile-group"))
-    )
-    elements = main.find_elements_by_class_name("dbg0pd")
-    for name in elements:
-        name.click()
-        address = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "Z1hOCe"))
-        )
-        rating = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "Ob2kfd"))
-        )
-        service = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.TAG_NAME, "c-wiz"))
-        )
-        Hours = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "TLou0b"))
-        )
-        contact = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "LrzXr.zdqRlf.kno-fv"))
-        )
-        print(name.text)
-        print(contact.text)
-        print(Hours.text)
-        print(service.text)
-        print(rating.text)
-        print(address.text)
-        print("")
-        driver.back()
-except:
-    print("name is ")'''
 
 view_all = WebDriverWait(driver, 90).until(
     EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "View all"))
@@ -103,43 +62,3 @@ for i in elements:
 
     print("")
 
-    '''service = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.TAG_NAME, "c-wiz"))
-    )
-    Hours = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "TLou0b"))
-    )
-    contact = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "LrzXr.zdqRlf.kno-fv"))
-    )'''
-
-# try to find eleement by partially link name
-'''
-elements = main.find_elements_by_class_name("dbg0pd")
-for name in elements:
-    time.sleep(10)
-    name.click()
-    print("kkkk")
-    address = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "Z1hOCe"))
-    )
-    rating = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "Ob2kfd"))
-    )
-    service = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.TAG_NAME, "c-wiz"))
-    )
-    Hours = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "TLou0b"))
-    )
-    contact = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "LrzXr.zdqRlf.kno-fv"))
-    )
-    print(name.text)
-    print(contact.text)
-    print(Hours.text)
-    print(service.text)
-    print(rating.text)
-    print(address.text)
-    print("")
-    driver.back()'''
